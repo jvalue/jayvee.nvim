f00ead23b7e1302c97b41e71635c05287c9fc48a
Jonas Zeltner <jonas.zeltner@posteo.de>
2024-12-19 17:51:04 +0100

    Replace deprecated method

--- lua/jayvee/init.lua
+++ lua/jayvee/init.lua
@@ -1,11 +1,15 @@
 local M = {}
-M.setup = function(opts)
+---Set up and enable the jayvee lanugage server
+---@param opts vim.lsp.Config Custom configuration options
+function M.setup(opts)
 	require("lspconfig.configs").jayvee_ls = {
 		default_config = {
 			cmd = { "jayvee-language-server", "--stdio" },
 			filetypes = { "jayvee" },
 			single_file_support = true,
-			root_dir = require("lspconfig.util").find_git_ancestor,
+			root_dir = function(startpath)
+				vim.fs.dirname(vim.fs.find(".git", { path = startpath, upward = true })[1])
+			end,
 		},
 	}
 	require("lspconfig").jayvee_ls.setup(opts)
