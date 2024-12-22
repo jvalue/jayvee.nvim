12bb889e7103340392cfa4c006bcf1a37efc498e
Jonas Zeltner <jonas.zeltner@posteo.de>
2024-12-22 19:37:53 +0100

    Fix executable check

--- lua/jayvee/health.lua
+++ lua/jayvee/health.lua
@@ -1,10 +1,10 @@
 local M = {}
 function M.check()
 	vim.health.start("jayvee.nvim")
-	if vim.fn.executable("jayvee-language-server") then
+	if vim.fn.executable("jayvee-language-server") == 1 then
 		vim.health.ok("Found executable for `jayvee-lanugage-server`")
 	else
-		vim.health.vim.health.error(
+		vim.health.error(
 			"Could not find executable for `jayvee-language-server`",
 			{ "You may not have installed the jayvee-language server" },
 			{ "The most convenient way to do so is via mason.nvim" }
