07a9b2cf25dda45df4ead1c7d734e245b786fe3a
Jonas Zeltner <jonas.zeltner@posteo.de>
2024-12-22 19:43:45 +0100

    Link mason.nvim in warning message

--- lua/jayvee/health.lua
+++ lua/jayvee/health.lua
@@ -4,12 +4,9 @@ function M.check()
 	if vim.fn.executable("jayvee-language-server") == 1 then
 		vim.health.ok("Found executable for `jayvee-lanugage-server`")
 	else
-		vim.health.warn(
-			"Could not find executable for `jayvee-language-server`",
-			{
-				"You may not have installed the jayvee-language server. The most convenient way to do so is via mason.nvim",
-			}
-		)
+		vim.health.warn("Could not find executable for `jayvee-language-server`", {
+			"You may not have installed the jayvee-language server. The most convenient way to do so is via [mason.nvim](https://github.com/williamboman/mason.nvim).",
+		})
 	end
 end
 return M
