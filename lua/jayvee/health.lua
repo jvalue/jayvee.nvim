89729fce4d5e0c6a8d87bf1147ec2da7fe3580ed
Jonas Zeltner <jonas.zeltner@posteo.de>
2025-07-04 21:22:23 +0200

    Use the user's configuration to determine whether the server is installed or not

--- lua/jayvee/health.lua
+++ lua/jayvee/health.lua
@@ -1,11 +1,36 @@
 local M = {}
 function M.check()
 	vim.health.start("jayvee.nvim")
-	if vim.fn.executable("jayvee-language-server") == 1 then
-		vim.health.ok("Found executable for `jayvee-lanugage-server`")
+	local lsp_config = vim.lsp.config["jayvee-language-server"]
+	if not lsp_config then
+		vim.health.error(
+			"No configuration found for `jayvee-language-server`",
+			"This plugin already includes configuration for the language server"
+		)
+		return
+	end
+	local cmd = lsp_config.cmd
+	local name = lsp_config.name or "jayvee-language-server"
+
+	if not cmd then
+		vim.health.error(
+			("%s's configuration did not contain a `cmd` field"):format(name),
+			"The default configuration contains this field"
+		)
+		return
+	end
+
+	if type(cmd) == "function" then
+		return
+	end
+
+	if vim.fn.executable(cmd[1]) == 1 then
+		vim.health.ok(("Found executable for `%s`"):format(name))
 	else
-		vim.health.warn("Could not find executable for `jayvee-language-server`", {
-			"You may not have installed the jayvee-language server. The most convenient way to do so is via [mason.nvim](https://github.com/williamboman/mason.nvim).",
+		vim.health.warn(("Could not find executable for `%s`"):format(name), {
+			("You may not have `%s` installed. The most convenient way to do so is via [mason.nvim](https://github.com/williamboman/mason.nvim)."):format(
+				name
+			),
 		})
 	end
 end
